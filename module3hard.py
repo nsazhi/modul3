6964a371edfa83a2d8d7bea6801328f9b1555cfc
nsazhi <n190275@inbox.ru>
2024-07-30 12:00:04 +0400

    module3hard.py

--- module3hard.py
+++ module3hard.py
@@ -3,29 +3,24 @@ def calculate_structure_sum(*args):
     for i in args:
         if isinstance(i, int):
             calc += i
-        elif isinstance(i, str):
+            continue
+        if isinstance(i, str):
             calc += len(i)
-        elif isinstance(i, dict):
-            for key, value in i.items():
-                if isinstance(key, int):
-                    calc += key
-                if isinstance(key, str):
-                    calc += len(key)
-                if isinstance(value, int):
-                    calc += value
-                if isinstance(value, str):
-                    calc += len(value)
+            continue
+        if isinstance(i, dict):
+            dict_zip = zip(i.keys(), i.values())
+            calculate_structure_sum(*dict_zip)
         else:
             calculate_structure_sum(*i)
     return calc
 
 
 data_structure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 calc = 0
